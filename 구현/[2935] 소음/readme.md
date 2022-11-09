a52664b35eb28e3d8c80f752796e5c86f3c7db83
김민찬 <81581362+nsmchan@users.noreply.github.com>
2022-11-09 20:23:13 +0900

    브론즈 구현

---
+++ 구현/[2935] 소음/readme.md
@@ -0,0 +1,7 @@
+##  백준 Bronze III  [2935] 소음
+
+* 수학
+* 문자열
+* 사칙연산
+ 
+[문제 바로가기](https://www.acmicpc.net/problem/2935)
--- 구현/[2935] 소음/김민찬/boj2935.java
+++ 구현/[2935] 소음/김민찬/boj2935.java
@@ -1,37 +1,29 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 
 
 public class boj2935{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BigInteger A = new BigInteger(br.readLine());
+		String c = br.readLine();
+		BigInteger B = new BigInteger(br.readLine());
 
-	     public static void main(String[] args) throws IOException{
-	    	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	   
-		     int N = Integer.parseInt(br.readLine());
-             StringTokenizer st = new StringTokenizer(br.readLine());
-             StringTokenizer st2 = new StringTokenizer(br.readLine());
-             
-
-		     int A[] = new int[N];
-		     for(int i=0;i<N;i++) {
-		    	 A[i]= Integer.parseInt(st.nextToken());
-		     }	     
-		     int B=Integer.parseInt(st2.nextToken());
-		     int C=Integer.parseInt(st2.nextToken());
-
-		        long sum=N;
-		        for(int i=0;i<N;i++){
-		            A[i]=A[i]-B;
-	                if(A[i]<0) A[i]=0;
-		            if(A[i]%C!=0){
-		                sum=sum+A[i]/C+1; 
-		            }
-		            else{
-		                sum=sum+A[i]/C;
-		            }
-		        }
-		        System.out.println(sum);
-		    }
+		if(c.equals("*")){
+			bw.write(A.multiply(B)+"");
+		}
+		else{
+			bw.write(A.add(B)+"");
+		}
+		bw.flush();
+		bw.close();
+		br.close();
+	}
 }
+
       
